e40786e81596e2b035b0496147665b40cae89e08
ItsMeZy <zyrilerrol.gatchalian@student.supsi.ch>
2024-01-20 19:31:34 +0100

    add files

---
+++ CollaborativeFiltering/CollaborativeFilteringSimple.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using System;
+using System.IO;
+
+class CollaborativeFilteringSimple
+{
+    public class MovieRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float movieId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+
+    static void Main(string[] args)
+    {
+        MLContext mlContext = new MLContext();
+
+        (IDataView trainingDataView, IDataView testDataView) = LoadData(mlContext);
+
+        ITransformer model = BuildAndTrainModel(mlContext, trainingDataView);
+
+        Console.WriteLine("=============== Evaluating the model ===============");
+        var prediction = model.Transform(testDataView);
+        var metrics = mlContext.Regression.Evaluate(prediction, labelColumnName: "Label", scoreColumnName: "Score");
+        Console.WriteLine("Root Mean Squared Error : " + metrics.RootMeanSquaredError.ToString());
+        Console.WriteLine("RSquared: " + metrics.RSquared.ToString());
+    }
+
+    static (IDataView training, IDataView test) LoadData(MLContext mlContext)
+    {
+        var trainingDataPath = Path.Combine(Environment.CurrentDirectory+"\\..\\..\\..\\train.csv");
+        var testDataPath = Path.Combine(Environment.CurrentDirectory+"\\..\\..\\..\\test.csv");
+
+        IDataView trainingDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+        IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+
+        return (trainingDataView, testDataView);
+    }
+
+    static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainingDataView)
+    {
+        IEstimator<ITransformer> estimator = mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "userIdEncoded", inputColumnName: "userId")
+    .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "movieIdEncoded", inputColumnName: "movieId"));
+
+        var options = new MatrixFactorizationTrainer.Options
+        {
+            MatrixColumnIndexColumnName = "userIdEncoded",
+            MatrixRowIndexColumnName = "movieIdEncoded",
+            LabelColumnName = "Label",
+            NumberOfIterations = 100,
+            ApproximationRank = 100
+        };
+
+        var trainerEstimator = estimator.Append(mlContext.Recommendation().Trainers.MatrixFactorization(options));
+
+        Console.WriteLine("=============== Training the model ===============");
+        ITransformer model = trainerEstimator.Fit(trainingDataView);
+
+        return model;
+    }
+}
---
+++ ReRecommendationSystem/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Accord.MachineLearning.Rules;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random r = new Random(100);
+        List<SortedSet<int>> dataset = new List<SortedSet<int>>();
+
+        // Generate 100 transaction of max
+
+        int numTransaction = 50;
+        int numItems = 5;
+        for (int i = 0; i < numTransaction; i++)
+        {
+            SortedSet<int> t;
+            do
+            {
+                t = new SortedSet<int>(Enumerable.Range(2, r.Next(numItems)).Select(x => r.Next(numItems)).ToList().Distinct().ToList());
+            } while (t.Count < 2);
+            dataset.Add(t);
+        }
+
+        // Create a new A-priori learning algorithm with the requirements
+        var apriori = new Apriori(threshold: 3, confidence: 0);
+
+        // Use the algorithm to learn a set matcher
+        AssociationRuleMatcher<int> classifier = apriori.Learn(dataset.ToArray<SortedSet<int>>());
+
+        // Use the classifier to find orders that are similar to 
+        // orders where clients have bought items 1 and 2 together:
+        int[][] matches = classifier.Decide(new[] { 1, 2 });
+
+        foreach(var l in dataset)
+        {
+            Console.WriteLine(string.Join(",",l));
+        }
+
+        AssociationRule<int>[] rules = classifier.Rules;
+        Console.ReadLine();
+        //3 0 4
+    }
+}
